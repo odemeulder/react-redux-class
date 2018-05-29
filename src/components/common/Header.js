ca995879d75556aa4b9fb8f64184942ceb7f3f8d
odemeulder-nyt <olivier.demeulder@nytimes.com>
2018-05-29 08:52:00 -0400

    header modifications

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,24 +1,37 @@
 import React, { PropTypes } from 'react';
 import { Link, IndexLink } from 'react-router';
 import LoadingDots from './LoadingDots';
+import { connect } from 'react-redux'
 
-const Header = ({loading}) => {
-  return (
-    <nav>
-      <IndexLink to="/" activeClassName="active">Home</IndexLink>
-      {" | "}
-      <Link to="/courses" activeClassName="active">Courses</Link>
-      {" | "}
-      <Link to="/authors" activeClassName="active">Authors</Link>
-      {" | "}
-      <Link to="/about" activeClassName="active">About</Link>
-      {loading && <LoadingDots interval={100} dots={20} />}
-    </nav>
-  );
-};
+class Header extends React.Component {
+
+  render() {
+    return (
+      <nav>
+        <IndexLink to="/" activeClassName="active">Home</IndexLink>
+        {" | "}
+        <Link to="/courses" activeClassName="active">Courses</Link>{this.props.courseCount ? ` (${this.props.courseCount})` : ''}
+        {" | "}
+        <Link to="/authors" activeClassName="active">Authors</Link>{this.props.authorCount ? ` (${this.props.authorCount})` : ''}
+        {" | "}
+        <Link to="/about" activeClassName="active">About</Link>
+        {this.props.loading && <LoadingDots interval={100} dots={20} />}
+      </nav>      
+    )
+  }
+}
 
 Header.propTypes = {
-  loading: React.PropTypes.bool.isRequired
+  loading: React.PropTypes.bool.isRequired,
+  authorCount: React.PropTypes.number,
+  courseCount: React.PropTypes.number
 };
 
-export default Header;
+function mapStateToProps(state) {
+  return {
+    'authorCount': state.authors.length,
+    'courseCount': state.courses.length
+  }
+}
+
+export default connect(mapStateToProps)(Header);
